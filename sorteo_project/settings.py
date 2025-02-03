8194d3ffe11f4661b209bb19c51b635c46f42f36
Luciano Zurlo <luciano@aiMac.local>
2025-02-03 19:04:48 -0300

    all

--- sorteo_project/settings.py
+++ sorteo_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "whitenoise.runserver_nostatic"
+    "whitenoise.runserver_nostatic"
     "sorteo_app"
 ]
 
@@ -123,5 +124,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'sorteo_app/static')
+]
+
+
 
 CSRF_TRUSTED_ORIGINS = ["https://web-production-0252.up.railway.app"]
