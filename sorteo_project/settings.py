a174ce66ee59e36a1bcee254e1d02f10c9f23874
Luciano Zurlo <luciano@aiMac.local>
2025-02-03 19:54:43 -0300

    all

--- sorteo_project/settings.py
+++ sorteo_project/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +81,21 @@ WSGI_APPLICATION = "sorteo_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,  # Opcional: Tiempo máximo de conexión
+        ssl_require=True   # Necesario en Railway para habilitar conexiones seguras
+    )
 }
+
 
 
 # Password validation
