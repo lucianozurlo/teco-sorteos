5ccbda334e0e3c0e0ee78fc74b62daaf480403a4
Luciano Zurlo <luciano@aiMac.local>
2025-02-03 19:20:37 -0300

    all

--- sorteo_project/settings.py
+++ sorteo_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "whitenoise.runserver_nostatic"
+    "whitenoise.runserver_nostatic",
     "sorteo_app"
 ]
 
