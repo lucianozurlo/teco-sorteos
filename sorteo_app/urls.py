b080503b76dce95498b95efd9ea12174272710ee
Luciano Zurlo <luciano@aiMac.local>
2025-02-04 11:21:50 -0300

    all

--- sorteo_app/urls.py
+++ sorteo_app/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from sorteo_app.views import render_articles
+# from django.urls import path
+# from sorteo_app.views import render_articles
 
-urlpatterns = [
-    path('', render_articles, name='articles')
-]
+# urlpatterns = [
+#     path('', render_articles, name='articles')
+# ]
